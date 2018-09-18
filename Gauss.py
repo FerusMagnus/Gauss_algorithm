37943e7b290bce76018411e0b5cea7e48a2ab9b0
FerusMagnus <38263282+FerusMagnus@users.noreply.github.com>
2018-09-18 16:25:07 +0300

    Final version

--- Gauss.py
+++ Gauss.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
-import sys
-
+# a = [[1,5,-1,7], [2,-1,-1,4], [3,-2,4,11]]
+# n = 3
 print('Здравствуйте')
 n = int(input('Введите количество строк матрицы: '))
 
 print('Введите СЛАУ в матричном виде')
-a = [[0] * n+1 for i in range(n)]  # двумерный массив заполненный нулями
+a = [[0 for j in range(n+1)] for i in range(n)]  # двумерный массив заполненный нулями
 for i in range(n):
     for j in range(n+1):
-        if j == n+1:
+        if j == n:
             message = 'b' + str(i + 1)
             a[i][j] = int(input(message + ' '))
         else:
@@ -25,13 +25,13 @@ for k in range(1, n):
 
 # Обратный ход
 
-x = [0] * n  # список полученных коэффициентов
-for i in range(n-1, 0, -1):
+x = [0 for i in range(n)]  # список полученных коэффициентов
+for i in range(n-1, -1, -1):
     x[i] = a[i][n] / a[i][i]
     for c in range(n-1, i, -1):
         x[i] = x[i] - a[i][c] * x[c] / a[i][i]
 
-result = 'Результат'
+result = 'Результат: '
 for i in range(n):
     result = result + 'x' + str(i + 1) + '=' + str(x[i]) + '; '
 
